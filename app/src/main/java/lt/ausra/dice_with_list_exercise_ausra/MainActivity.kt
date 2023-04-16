27af7619da46f0ec64f97c8e0a7c023e2e31b1ee
apaskauskyte <a.paskauskyte@gmail.com>
2023-04-16 15:33:00 +0300

    Initial commit

---
+++ app/src/main/java/lt/ausra/dice_with_list_exercise_ausra/MainActivity.kt
@@ -0,0 +1,11 @@
+package lt.ausra.dice_with_list_exercise_ausra
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
