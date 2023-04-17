637d595168e7bae654d89fb57477f7cf669ba480
apaskauskyte <a.paskauskyte@gmail.com>
2023-04-17 13:33:49 +0300

    Add function moreDice

--- app/src/main/java/lt/ausra/dice_with_list_exercise_ausra/MainActivity.kt
+++ app/src/main/java/lt/ausra/dice_with_list_exercise_ausra/MainActivity.kt
@@ -2,6 +2,7 @@ package lt.ausra.dice_with_list_exercise_ausra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.core.view.isInvisible
@@ -14,7 +15,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var fourthDice: TextView
     lateinit var fifthDice: TextView
     lateinit var sixthDice: TextView
+    lateinit var seventhDice: TextView
+    lateinit var eightDice: TextView
+
     lateinit var sumOfDice: TextView
+
     lateinit var rollDiceButton: Button
     lateinit var moreDiceButton: Button
     lateinit var lessDiceButton: Button
@@ -22,41 +27,57 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rollDiceButton = findViewById(R.id.rollDiceButton)
-        moreDiceButton = findViewById(R.id.moreDiceButton)
-        lessDiceButton = findViewById(R.id.lessDiceButton)
         firstDice = findViewById(R.id.firstDice)
         secondDice = findViewById(R.id.secondDice)
         thirdDice = findViewById(R.id.thirdDice)
         fourthDice = findViewById(R.id.fourthDice)
         fifthDice = findViewById(R.id.fifthDice)
         sixthDice = findViewById(R.id.sixthDice)
+        seventhDice = findViewById(R.id.seventhDice)
+        eightDice = findViewById(R.id.eightDice)
+
         sumOfDice = findViewById(R.id.sumOfDice)
+
+        rollDiceButton = findViewById(R.id.rollDiceButton)
+        moreDiceButton = findViewById(R.id.moreDiceButton)
+        lessDiceButton = findViewById(R.id.lessDiceButton)
+
+        seventhDice.visibility = View.GONE
+        eightDice.visibility = View.GONE
 
         rollDiceButton.setOnClickListener {
             rollDice()
+        }
+
+        moreDiceButton.setOnClickListener {
+            moreDice()
         }
     }
 
     private fun rollDice() {
 
-        val diceValuesList = mutableListOf<Int>()
+        val diceValues = mutableListOf<Int>()
         var diceSum = 0
-
-        for (i in 1..6) {
-            val diceRoll = (1..6).random()
-            diceValuesList.add(diceRoll)
-            diceSum += diceRoll
+        val diceHighestValue = 6
+        val diceTextViews = arrayOf(
+            firstDice, secondDice, thirdDice, fourthDice, fifthDice, sixthDice, seventhDice, eightDice
+        )
+        for (i in 1 .. diceTextViews.size) {
+            val diceRoll = (1..diceHighestValue).random()
+            diceValues.add(diceRoll)
+            diceTextViews[i - 1].text = diceValues[i - 1].toString()
+            if (diceTextViews[i - 1].isShown) {
+                diceSum += diceRoll
+            }
         }
-
-        firstDice.text = diceValuesList[0].toString()
-        secondDice.text = diceValuesList[1].toString()
-        thirdDice.text = diceValuesList[2].toString()
-        fourthDice.text = diceValuesList[3].toString()
-        fifthDice.text = diceValuesList[4].toString()
-        sixthDice.text = diceValuesList[5].toString()
         sumOfDice.text = diceSum.toString()
     }
-
+    private fun moreDice() {
+        if (!seventhDice.isShown) {
+            seventhDice.visibility = View.VISIBLE
+        } else {
+            eightDice.visibility = View.VISIBLE
+        }
+    }
 }
 
